1746a02899ef29d190efaf2d94f9934d5e0c6d58
Andre Perez <andremztm@gmail.com>
2024-11-24 19:52:58 -0300

    chore: add favicon

--- balneabilidade/settings.py
+++ balneabilidade/settings.py
@@ -148,7 +148,15 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "balneabilidade", "assets"),
+)
+
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
