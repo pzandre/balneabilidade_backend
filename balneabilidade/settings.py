a72891e3d3be555004d605e303a7a21e6becd447
Andre Perez <andremztm@gmail.com>
2024-11-25 08:00:03 -0300

    chore: remove dotenv

--- balneabilidade/settings.py
+++ balneabilidade/settings.py
@@ -13,13 +13,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-try:
-    from dotenv import load_dotenv  # noqa
-
-    load_dotenv()
-except ImportError:
-    pass
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
