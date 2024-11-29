bd86de853ff5ae14b4cc6e3edca5a5973760ce59
Andre Perez <andremztm@gmail.com>
2024-11-29 10:25:15 -0300

    chore: fix http method

--- cronjob_scripts/updateLocation.ts
+++ cronjob_scripts/updateLocation.ts
@@ -1,6 +1,13 @@
 export default async function(interval: Interval) {
-  await fetch(Deno.env.get("BACKEND_URL") + "management/weather/", {
+  await fetch(Deno.env.get("BACKEND_URL") + "management/locations/", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Api-Key ${Deno.env.get("BACKEND_API_KEY")}`,
+    },
+  });
+  await fetch(Deno.env.get("BACKEND_URL") + "locations/", {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Api-Key ${Deno.env.get("BACKEND_API_KEY")}`,
--- cronjob_scripts/updateWeather.ts
+++ cronjob_scripts/updateWeather.ts
@@ -1,6 +1,13 @@
 export default async function(interval: Interval) {
-  await fetch(Deno.env.get("BACKEND_URL") + "management/locations/", {
+  await fetch(Deno.env.get("BACKEND_URL") + "management/weather/", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Api-Key ${Deno.env.get("BACKEND_API_KEY")}`,
+    },
+  });
+  await fetch(Deno.env.get("BACKEND_URL") + "weather/", {
+    method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Api-Key ${Deno.env.get("BACKEND_API_KEY")}`,
